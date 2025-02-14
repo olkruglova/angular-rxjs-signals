07e427d279c48029b765a1106cd7434a4edb4356
Olga Kruglova <olha.kruhlova@inetum.com>
2025-02-14 17:19:30 +0100

    of/from creation options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { from, of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'angular-rxjs-signals';
+
+  subscription: Subscription = new Subscription();
+
+    ngOnInit() {
+        this.subscription.add(
+            of(2,4,6,8).subscribe(item => console.log(item))
+        )
+
+        this.subscription.add(
+            of([2,4,6,8]).subscribe(arr => console.log(arr))
+        )
+
+        this.subscription.add(
+            from([10, 20, 30, 40]).subscribe({
+                next: item => console.log(item),
+                error: err => console.error(err),
+                complete: () => console.log('Complete')
+            })
+        )
+    
+        this.subscription.add(
+            of("aaa", "bbb", "ccc").subscribe({
+                next: item => console.log(item),
+                error: err => console.error(err),
+                complete: () => console.log('Complete')
+            })
+        )
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
 }
