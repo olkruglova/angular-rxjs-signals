07bfd6b1a5302e97ac249e0bb17fd41b0e635f5f
Olga Kruglova <olha.kruhlova@inetum.com>
2025-02-16 19:15:48 +0100

    caching with shareReplay

--- src/app/demo/demo.service.ts
+++ src/app/demo/demo.service.ts
@@ -1,6 +1,15 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, concatMap, from, map, of, range, tap } from 'rxjs';
+import {
+  catchError,
+  concatMap,
+  from,
+  map,
+  of,
+  range,
+  shareReplay,
+  tap,
+} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,10 +19,11 @@ export class DemoService {
 
   private http = inject(HttpClient);
 
-  productsResult$ = of([1, 2, 3, 4, 5]).pipe(
+  readonly productsResult$ = of([1, 2, 3, 4, 5]).pipe(
     tap((x) => console.log('Before map:', x)),
     map((data) => ({ data })),
     tap((x) => console.log('After map:', x)),
+    shareReplay(1),
     catchError((err: HttpErrorResponse) => {
       console.error(err);
       return of({ data: [], error: 'An error occurred' });
